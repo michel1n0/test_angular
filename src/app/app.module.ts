acdd09fc8d68fdfa69264bc05afbce612ab418f4
michel1n0 <fpaucar.caceres@gmail.com>
2022-11-09 17:56:23 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompaComponent } from './compa/compa.component';
+import { CompbComponent } from './compb/compb.component';
+import { CompcComponent } from './compc/compc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaComponent,
+    CompbComponent,
+    CompcComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/compa/compa.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compa',
+  templateUrl: './compa.component.html',
+  styleUrls: ['./compa.component.css']
+})
+export class CompaComponent {
+
+}
